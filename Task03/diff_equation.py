9f6bb71b63f5a9d0585bc173d692645822f4e4ce
Ananias <86498679+ananiasmachado@users.noreply.github.com>
2023-06-17 09:33:23 -0300

    Adding Example Usage Exercise 5

--- Task03/diff_equation.py
+++ Task03/diff_equation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.constants import g as acceleration_gravity
 
 class DiffEquationSolver:
     def __init__(self, F, t0, y0, dy0):
@@ -75,7 +76,9 @@ class DiffEquationSolver:
         return yk, N, points
 
 if __name__ == "__main__":
+    #
     # Example usage -> Exercise 4 from Task 03
+    #
 
     # Differential equation F(t) = y''(t) = f(t, y(t), y'(t))
     def F(t, y, dy):
@@ -95,6 +98,51 @@ if __name__ == "__main__":
     # Step size and integration time
     h = 0.01
     T = 25
+
+    solver = DiffEquationSolver(F, t0, y0, dy0)
+
+    # Taylor's method
+    solution_taylor, iter_taylor, points_taylor = solver.taylor(h, T)
+    print(f"\nSolution using Taylor's method: {solution_taylor}")
+    print(f"Number of iterations: {iter_taylor}")
+
+    plt.plot(points_taylor[0], points_taylor[1])
+    plt.title("Taylor's method solution plot")
+    plt.xlabel("t")
+    plt.ylabel("y")
+    plt.show()
+
+    # Runge-Kutta Nystron's method
+    solution_runge_kutta, iter_runge_kutta, points_runge_kutta = solver.runge_kutta_nystron(h, T)
+    print(f"\nSolution using Runge-Kutta Nystron's method: {solution_runge_kutta}")
+    print(f"Number of iterations: {iter_runge_kutta}")
+
+    plt.plot(points_runge_kutta[0], points_runge_kutta[1])
+    plt.title("Runge-Kutta Nystron's method solution plot")
+    plt.xlabel("t")
+    plt.ylabel("y")
+    plt.show()
+
+    #
+    # Example usage -> Exercise 5 from Task 03
+    #
+
+    # Differential equation F(t) = y''(t) = f(t, y(t), y'(t))
+    def F(t, y, dy):
+        kd = 1
+        G = (-1) * acceleration_gravity - kd * dy * np.abs(dy)
+        return G
+
+    print(acceleration_gravity)
+
+    # Initial conditions
+    t0 = 0
+    y0 = 0
+    dy0 = 0
+
+    # Step size and integration time
+    h = 0.01
+    T = 20
 
     solver = DiffEquationSolver(F, t0, y0, dy0)
 
--- Task03/wolfram_testbench.md
+++ Task03/wolfram_testbench.md
@@ -1,6 +1,9 @@
 # WolframAlpha Testbench
 
-The following script was used to generate the solution plot for Exercise 4 from Task 03:
+### Questão 4
 
-Runge-Kutta method, d^2y/dx^2 = 2 * sin(0.5 * x) + sin(2 * 0.5 * x) + cos(3 * 0.5 * x) - 0.2 * dy/dx - 1 * y, y(0) = 2, y'(0) = 2, from 1 to 25, h = .01
+Runge-Kutta method: https://www.wolframalpha.com/input?i=Runge-Kutta+method%2C+d%5E2y%2Fdx%5E2+%3D+2+*+sin%280.5+*+x%29+%2B+sin%282+*+0.5+*+x%29+%2B+cos%283+*+0.5+*+x%29+-+0.2+*+dy%2Fdx+-+1+*+y%2C+y%280%29+%3D+2%2C+y%27%280%29+%3D+2%2C+from+0+to+25%2C+h+%3D+.01
 
+### Questão 5
+
+Runge-Kutta method: https://www.wolframalpha.com/input?i=Runge-Kutta+method%2C+d%5E2y%2Fdx%5E2+%3D+-9.80665+-1*dy%2Fdx*%7Cdy%2Fdx%7C%2C+y%280%29+%3D+0%2C+y%27%280%29+%3D+0%2C+from+0+to+20%2C+h+%3D+.01
